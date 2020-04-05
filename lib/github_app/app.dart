676a23d5fc433bd6273fe43f231affea4b2a9c19
liyakun <1315385515@qq.com>
2020-04-05 11:57:33 +0800

    update

---
+++ lib/github_app/app.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class FlutterReduxApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'gallery/app.dart';
+// import 'github_app/app.dart';
 
 void main() {
-  runApp(const GalleryApp());
+  // runApp(const GalleryApp());
+  runZoned(
+    () {
+      runApp(const GalleryApp());
+      PaintingBinding.instance.imageCache.maximumSize = 100;
+      Provider.debugCheckInvalidValueType = null;
+    },
+    onError: (Object obj, StackTrace stack) {
+      print(obj);
+      print(stack);
+    },
+  );
 }
